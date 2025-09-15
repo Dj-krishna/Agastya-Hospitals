02999ef0ebdf6500882fe7d442877c7d5fbba721
DJ <12krishna.jaya@gamil.com>
2025-09-15 12:25:06 +0530

    updated the code

--- Frontend/src/pages/Blog.jsx
+++ Frontend/src/pages/Blog.jsx
@@ -1,55 +1,74 @@
 import { useEffect } from "react";
 import { useState } from "react";
-import { BLOGS_API } from "../api/services";
-import axios from "axios";
 import { format } from "date-fns";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchBlogs } from "../slices/blogSlice";
 
 const Blog = () => {
-  const [blogsData, setBlogsData] = useState([]);
+  // const [blogsData, setBlogsData] = useState([]);
 
-  const fetchBlogs = async () => {
-    const response = await axios.get(BLOGS_API);
-    setBlogsData(response.data);
-    console.log(response.data);
-  };
+  const dispatch = useDispatch();
+  const {
+    data: blogs,
+    loading,
+    error,
+  } = useSelector((state) => {
+    console.log(state);
+    return state.blogs.blogs;
+  });
 
   useEffect(() => {
-    fetchBlogs();
-  }, []);
+    dispatch(fetchBlogs());
+  }, [dispatch]);
+
+  console.log("BLOGS DATA ", blogs);
 
   return (
     <div className="container p-5">
-      <div className="row mx-5">
-        <div className="col-lg-12">
-          <div className="blog-list">
-            {blogsData.data?.map((blog) => (
-              <div
-                key={blog.blogID}
-                className="blog-card medical-news shadow-sm border"
-              >
-                <div className="h-48 bg-gradient-to-br from-blue-100 to-blue-200 flex items-center justify-center mb-3">
-                  <span className="text-6xl">
-                    <img src={blog.postThumbnail} />
-                  </span>
+      {loading ? (
+        <div className="text-center">
+          <div
+            className="spinner-grow text-primary"
+            style={{ width: "3rem", height: "3rem" }}
+            role="status"
+          >
+            <span className="visually-hidden">Loading...</span>
+          </div>
+          <p className="text-center">Loading...</p>
+        </div>
+      ) : (
+        <div className="row mx-5">
+          <div className="col-lg-12">
+            <div className="blog-list">
+              {blogs?.map((blog) => (
+                <div
+                  key={blog.blogID}
+                  className="blog-card medical-news shadow-sm border"
+                >
+                  <div className="h-48 bg-gradient-to-br from-blue-100 to-blue-200 flex items-center justify-center mb-3">
+                    <span className="text-6xl">
+                      <img src={blog.postThumbnail} />
+                    </span>
+                  </div>
+                  <div className="blog-meta">
+                    <span>{blog.category}</span>
+                    <span>
+                      {format(new Date(blog.dateOfPost), "dd-MMM-yyyy")}
+                    </span>
+                  </div>
+                  <h3 className="mb-5">
+                    <a href="#">{blog.title}</a>
+                  </h3>
+                  {/* <p className="excerpt"> {blog.excerpt}</p> */}
+                  <a href="#" className="text-primary f-12">
+                    Read the article →
+                  </a>
                 </div>
-                <div className="blog-meta">
-                  <span>{blog.category}</span>
-                  <span>
-                    {format(new Date(blog.dateOfPost), "dd-MMM-yyyy")}
-                  </span>
-                </div>
-                <h3 className="mb-5">
-                  <a href="#">{blog.title}</a>
-                </h3>
-                {/* <p className="excerpt"> {blog.excerpt}</p> */}
-                <a href="#" className="text-primary f-12">
-                  Read the article →
-                </a>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
--- Frontend/src/store.js
+++ Frontend/src/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import doctorReducer from "./slices/doctorsSlice";
+import blogReducer from "./slices/blogSlice";
 
 const store = configureStore({
   reducer: {
     doctors: doctorReducer,
+    blogs: blogReducer,
   },
 });
 
