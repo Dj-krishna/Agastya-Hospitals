443515e66dfde57052f3953dacc4b15ee6b3c3cb
DJ <12krishna.jaya@gamil.com>
2025-10-05 16:36:53 +0530

    updated the blogs

--- admin-website/src/Components/Appointments/index.jsx
+++ admin-website/src/Components/Appointments/index.jsx
@@ -1,23 +1,17 @@
 import React, { Fragment, useEffect, useState } from "react";
 import { Badges, Breadcrumbs } from "../../AbstractElements";
 import { Container, Row } from "reactstrap";
-import TableComponent from "../Common/Component/TableComponent";
 import axios from "axios";
 import { APPOINTMENTS_API } from "../../api";
-import { format } from "date-fns";
 import AppointmentsForm from "./AppointmentsForm";
 import AppointmentsTable from "./AppointmentsTable";
 import TableSkeleton from "../Common/Component/TableSkeleton";
-import PaginationComponent from "../Common/Component/PaginationComponent";
 
-const today = new Date();
-const ITEMS_PER_PAGE = 7;
 const Appointments = () => {
   const [appointments, setAppointments] = useState([]);
   const [showAddAppointment, setShowAddAppointment] = useState(false);
   const [searchDate, setSearchDate] = useState("");
   const [isLoading, setIsLoading] = useState(false);
-  const [currentPage, setCurrentPage] = useState(1);
 
   const handleSearch = (date) => {
     setSearchDate(date);
@@ -44,20 +38,7 @@ const Appointments = () => {
   useEffect(() => {
     fetchAppointments();
   }, []);
-
-  // const totalPages = Math.ceil(appointments.length / ITEMS_PER_PAGE);
-  // const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
-  // const currentData = appointments.slice(
-  //   startIndex,
-  //   startIndex + ITEMS_PER_PAGE
-  // );
-
-  // const handlePageChange = (page) => {
-  //   if (page >= 1 && page <= totalPages) {
-  //     setCurrentPage(page);
-  //   }
-  // };
-
+  
   return (
     <Fragment>
       {!showAddAppointment ? (
